f9c28928b714806246ea4b0137a1fcefa79c84c0
mahavenkatvas <46440663+mahavenkatvas@users.noreply.github.com>
2019-08-27 21:44:15 -0900

    Create left pyramid pattern.java

---
+++ left pyramid pattern.java
@@ -0,0 +1,14 @@
+class sample
+{
+	public static void main(String[] arg)
+	{
+		for(int i=0;i<5;i++)
+		{
+		for (int j=0;j<=i;j++)
+		{
+		System.out.print("*");
+		}
+		System.out.println();
+		}
+	}
+}
