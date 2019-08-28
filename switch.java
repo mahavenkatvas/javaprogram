86ec0446fab3657d4425c54409aa1105cd0b1874
mahavenkatvas <46440663+mahavenkatvas@users.noreply.github.com>
2019-08-27 21:36:45 -0900

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,26 @@
+class sample
+{
+	public static void main(String[] arg)
+	{
+		int s=30;
+		String a="";
+		switch(s)
+		{
+			case 10:
+				a="ten";
+				break;
+			case 20:
+				a="twenty";
+				break;
+		    case 30:
+		    	a="thirty";
+		    	break;
+		    default:
+		        a="not exist";
+		    break;
+				
+				
+		}
+		System.out.println(a);
+		
+	}
